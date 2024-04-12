929bf43dde161a24acae90cf01995aa510788115
TudorMarian <marian_tudor17@yahoo.com>
2024-04-12 23:05:19 +0300

    Adaugare ambele teme

---
+++ LibraryManager/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace LibraryManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.btnAddBook = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnBorrowBook = new System.Windows.Forms.Button();
+            this.btnReturnBook = new System.Windows.Forms.Button();
+            this.dgvBooks = new System.Windows.Forms.DataGridView();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(18, 43);
+            this.txtTitle.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(270, 26);
+            this.txtTitle.TabIndex = 0;
+            this.txtTitle.TextChanged += new System.EventHandler(this.txtTitle_TextChanged);
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(18, 112);
+            this.txtAuthor.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(270, 26);
+            this.txtAuthor.TabIndex = 1;
+            this.txtAuthor.TextChanged += new System.EventHandler(this.txtAuthor_TextChanged);
+            // 
+            // btnAddBook
+            // 
+            this.btnAddBook.Location = new System.Drawing.Point(495, 108);
+            this.btnAddBook.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnAddBook.Name = "btnAddBook";
+            this.btnAddBook.Size = new System.Drawing.Size(112, 35);
+            this.btnAddBook.TabIndex = 2;
+            this.btnAddBook.Text = "Add";
+            this.btnAddBook.UseVisualStyleBackColor = true;
+            this.btnAddBook.Click += new System.EventHandler(this.btnAddBook_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(772, 52);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(112, 35);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnBorrowBook
+            // 
+            this.btnBorrowBook.Location = new System.Drawing.Point(633, 108);
+            this.btnBorrowBook.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnBorrowBook.Name = "btnBorrowBook";
+            this.btnBorrowBook.Size = new System.Drawing.Size(112, 35);
+            this.btnBorrowBook.TabIndex = 4;
+            this.btnBorrowBook.Text = "BorrowBook";
+            this.btnBorrowBook.UseVisualStyleBackColor = true;
+            this.btnBorrowBook.Click += new System.EventHandler(this.btnBorrowBook_Click);
+            // 
+            // btnReturnBook
+            // 
+            this.btnReturnBook.Location = new System.Drawing.Point(772, 108);
+            this.btnReturnBook.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnReturnBook.Name = "btnReturnBook";
+            this.btnReturnBook.Size = new System.Drawing.Size(112, 35);
+            this.btnReturnBook.TabIndex = 5;
+            this.btnReturnBook.Text = "ReturnBook";
+            this.btnReturnBook.UseVisualStyleBackColor = true;
+            this.btnReturnBook.Click += new System.EventHandler(this.btnReturnBook_Click);
+            // 
+            // dgvBooks
+            // 
+            this.dgvBooks.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvBooks.Location = new System.Drawing.Point(18, 205);
+            this.dgvBooks.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dgvBooks.Name = "dgvBooks";
+            this.dgvBooks.RowHeadersWidth = 62;
+            this.dgvBooks.Size = new System.Drawing.Size(360, 231);
+            this.dgvBooks.TabIndex = 6;
+            this.dgvBooks.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvBooks_CellContentClick);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(495, 56);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(250, 26);
+            this.txtSearch.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(158, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Introduceti titlul cartii:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(210, 20);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Introduceti numele autorului:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(491, 27);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(379, 20);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Introduceti detalii despre carte pentru a putea cauta:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 20);
+            this.label4.TabIndex = 12;
+            this.label4.Text = "Cautari gasite:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(968, 490);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.dgvBooks);
+            this.Controls.Add(this.btnReturnBook);
+            this.Controls.Add(this.btnBorrowBook);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnAddBook);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.txtTitle);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvBooks)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Button btnAddBook;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnBorrowBook;
+        private System.Windows.Forms.Button btnReturnBook;
+        private System.Windows.Forms.DataGridView dgvBooks;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ LibraryManager/Form1.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LibraryManager
+{
+    public partial class Form1 : Form
+    {
+        private Catalog _catalog;
+        private EventNotifier _eventNotifier;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            _catalog = Catalog.Instance;
+            _eventNotifier = new EventNotifier();
+        }
+
+        private void btnAddBook_Click(object sender, EventArgs e)
+        {
+            string title = txtTitle.Text;
+            string author = txtAuthor.Text;
+
+            Book newBook = new Book(title, author);
+            _catalog.AddBook(newBook);
+
+            _eventNotifier.Notify($"Book '{title}' by {author} added to the library.");
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string searchCriteria = txtSearch.Text;
+            List<Book> searchResults = _catalog.SearchBooks(searchCriteria);
+
+            dgvBooks.DataSource = null;
+            dgvBooks.DataSource = searchResults;
+        }
+
+        private void btnBorrowBook_Click(object sender, EventArgs e)
+        {
+            if (dgvBooks.SelectedRows.Count > 0)
+            {
+                Book selectedBook = (Book)dgvBooks.SelectedRows[0].DataBoundItem;
+                selectedBook.Borrow();
+                _eventNotifier.Notify($"Book '{selectedBook.Title}' borrowed successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Please select a book to borrow.");
+            }
+        }
+
+        private void btnReturnBook_Click(object sender, EventArgs e)
+        {
+            if (dgvBooks.SelectedRows.Count > 0)
+            {
+                Book selectedBook = (Book)dgvBooks.SelectedRows[0].DataBoundItem;
+                selectedBook.Return();
+                _eventNotifier.Notify($"Book '{selectedBook.Title}' returned successfully.");
+            }
+            else
+            {
+                MessageBox.Show("Please select a book to return.");
+            }
+        }
+
+        private void txtTitle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtAuthor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgvBooks_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+    }
+
+    public class Catalog
+    {
+        private static Catalog _instance;
+        private List<Book> _books;
+
+        private Catalog()
+        {
+            _books = new List<Book>();
+        }
+
+        public static Catalog Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Catalog();
+                return _instance;
+            }
+        }
+
+        public void AddBook(Book book)
+        {
+            _books.Add(book);
+        }
+
+        public void RemoveBook(Book book)
+        {
+            _books.Remove(book);
+        }
+
+        public List<Book> SearchBooks(string searchCriteria)
+        {
+            return _books.FindAll(book =>
+                book.Title.ToLower().Contains(searchCriteria.ToLower()) ||
+                book.Author.ToLower().Contains(searchCriteria.ToLower()));
+        }
+    }
+
+    public class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public bool IsBorrowed { get; private set; }
+
+        public Book(string title, string author)
+        {
+            Title = title;
+            Author = author;
+            IsBorrowed = false;
+        }
+
+        public void Borrow()
+        {
+            if (!IsBorrowed)
+            {
+                IsBorrowed = true;
+            }
+            else
+            {
+                MessageBox.Show("Book is already borrowed.");
+            }
+        }
+
+        public void Return()
+        {
+            if (IsBorrowed)
+            {
+                IsBorrowed = false;
+            }
+            else
+            {
+                MessageBox.Show("Book is not borrowed.");
+            }
+        }
+
+    }
+
+    public interface IObserver
+    {
+        void Update(string message);
+    }
+
+    public class EventNotifier
+    {
+        private List<IObserver> _observers = new List<IObserver>();
+
+        public void Attach(IObserver observer)
+        {
+            _observers.Add(observer);
+        }
+
+        public void Detach(IObserver observer)
+        {
+            _observers.Remove(observer);
+        }
+
+        public void Notify(string message)
+        {
+            foreach (var observer in _observers)
+            {
+                observer.Update(message);
+            }
+        }
+    }
+}
